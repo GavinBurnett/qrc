34ed5b906b39c535c7d74d84ca2f2bade4a22984
Gavin Burnett <gburnett@outlook.com>
2025-05-13 11:20:31 +0100

    tidy up formatting of linux function testing script

--- function_tests.sh
+++ function_tests.sh
@@ -18,15 +18,15 @@ else
     read -N 1 -p "Press any key to continue..."
     echo
     ./qrc --generate-keys
-     read -N 1 -p "Press any key to continue..."
+    read -N 1 -p "Press any key to continue..."
     echo
     ./qrc --show-key=$SECRET_KEY
     echo
     ./qrc --show-key=$PUBLIC_KEY
-     read -N 1 -p "Press any key to continue..."
+    read -N 1 -p "Press any key to continue..."
     echo
     ./qrc --validate-keys secret=$SECRET_KEY public=$PUBLIC_KEY
-      read -N 1 -p "Press any key to continue..."
+    read -N 1 -p "Press any key to continue..."
     echo
     ./qrc --encrypt key=$PUBLIC_KEY plaintext=$PLAINTEXT_FILE ciphertext=$CIPHERTEXT_FILE
     ls -l $CIPHERTEXT_FILE
@@ -47,9 +47,8 @@ else
     ./qrc --show-key=$SECRET_KEY
     echo
     ./qrc --show-key=$PUBLIC_KEY
-     read -N 1 -p "Press any key to continue..."
-    #echo
-   ./qrc --encrypt key=$PUBLIC_KEY plaintext=$PLAINTEXT_FILE ciphertext=$CIPHERTEXT_FILE
+    read -N 1 -p "Press any key to continue..."
+    ./qrc --encrypt key=$PUBLIC_KEY plaintext=$PLAINTEXT_FILE ciphertext=$CIPHERTEXT_FILE
     read -N 1 -p "Press any key to continue..."
     echo
     ./qrc --decrypt key=$SECRET_KEY ciphertext=$CIPHERTEXT_FILE plaintext=$DECODED_PLAINTEXT_FILE
